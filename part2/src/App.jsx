bbafefd64bed6b3b45f777cff849764113954ead
johnnydc7 <64507235+johnnydc7@users.noreply.github.com>
2025-08-20 18:40:01 -0500

    Part2: Mapping work

---
+++ part2/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Note from '../components/Note'
+
+
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <u1>
+        {notes.map(note => 
+        <Note key={note.id} note={note} />
+        )}
+      </u1>
+    </div>
+  )
+}
+
+export default App
